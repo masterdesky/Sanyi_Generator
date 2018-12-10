558a06807fa54d64b3669c7943ffc47fd0440baa
masterdesky <masterdesky@gmail.com>
2018-12-10 23:04:16 +0100

    first commit

---
+++ sanyi_words.py
@@ -0,0 +1,52 @@
+import random
+import string
+import numpy as np
+
+random_invocation = ['te sem fogod fel amiket írtam...',
+                     'nem is vártam mást tőletek...',
+                     'PÓC.',
+                     'tudod te, hogy kivel beszélsz?',
+                     'milyen bizonyítékod van erre?',
+                     'tudsz mást is a tananyag ismétélés kívül?',
+                     'BOLYGÓK NEM LÉTEZNEK!',
+                     'A nappali világosság nem a Nap fénye!',
+                     'A gravitációs hullámoknak nevezett jelenség csak elektromágneses retardáció!',
+                     'Einstein elmélete megbukott!',
+                     'Soha senki nem bizonyította még a Föld gömb mivoltát!',
+                     'Soha senki nem bizonyította még a gravitációnak nevezett jelenséget!',
+                     'Az igazi tudomány megfigyelésen alapul és kísérletileg igazolható. Nem matemágia! (mathemagic)',
+                     '']
+
+random_bad_adjective = ['gönbözs',
+                        'gonbozs',
+                        'gömbös',
+                        'GE',
+                        'fősodrású tudomány által szajkózott',
+                        'cégéi',
+                        'CGI',
+                        'komputer-generated',
+                        'szovterrel manipulált']
+
+random_good_adjective = ['saját FECORE készítésű',
+                         'önálló FECORE tervezésű',
+                         'a saját találmányaim alapján készült',
+                         '']
+
+random_bs_adjective = ['nagyfeszültséggel indukált',
+                       'geopotenciális',
+                       'gáznemű halmazállapotú',
+                       'fémhidrogénnel telített',
+                       'ionizált halmazállapotú',
+                       'folytonos színkibocsájtási spektrumú',
+                       'potenciál felszínű',
+                       'nagy mágneses terű',
+                       'undulációs']
+
+
+random_nouns = ['ring laser gyroscope',
+                'szoftver',
+                '']
+
+random_ending = ['Még kontemplálok rajta :)',
+                 'Később még polemizálhatunk ezen.',
+                 'Szeretnélek invitálni a témával kapcsolatban egy']
